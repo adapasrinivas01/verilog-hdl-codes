9c501df766a68e0e4d4fa7e81162d4401c268610
Naga Durga Srinivas Adapa <adapasrinivas2004@gmail.com>
2025-09-06 18:40:26 +0530

    Create port_and_module.v

---
+++ port_and_module.v
@@ -0,0 +1,9 @@
+// port and module
+module adder(input a,b,output sum,carry); 
+  // or input a,b;
+  //    output sum,carrry;
+endmodule
+
+// port is the declaration of input and output for the module (a,b,sum,carry)
+// module is the basic building block of verilog and it performs specific function
+  
